756b8c3301e23cbbf7033b07f8ff55a411b6eb3c
yoldashovmashrab <yoldashovmashrab6003@gmail.com>
2022-03-03 05:17:14 -0800

    descripton and page

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,12 +3,15 @@ import './main.css'
 
 function Main() {
 
+  const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
+
   const [data, setData] = useState([])
   const [page, setPage] = useState("1")
   const [input, setInput] = useState("spider")
+  const [theme, setTheme] = useState("light")
 
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=63eb2d11&s=${input || "spider"}&page=${page}`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=${page}&query="${input || "Spider"}`)
       .then(response => response.json())
       .then(result => setData(result))
       .catch(err => {
@@ -19,7 +22,7 @@ function Main() {
 
   const handleNextBtn = () => {
     setPage(x => {
-      if (x == Math.floor(data?.totalResults / 10)) {
+      if (x == data?.total_pages) {
         return x = 1
       }
       return ++x
@@ -29,46 +32,70 @@ function Main() {
   const handlePrevBtn = () => {
     setPage(x => {
       if (x == 1) {
-        return x = Math.floor(data?.totalResults / 10)
+        return x = data?.total_pages
       }
       return --x
     })
   }
 
+
+  console.log(data);
+
     return (
-      <div className="wrapper">
+                        <div className="wrapper">
+                        <div className="form-wrapper">
+                        <input type="text" onChange={(e) => setInput(e.target.value)} placeholder="Search..." /> 
 
-  <div className="form-wrapper">
-        <input type="text" onChange={(e) => setInput(e.target.value)} placeholder="Search..." /> 
-        
-    <div className="button-group">
-        <button onClick={handlePrevBtn}>
-            prev
-        </button>
+                        <button onClick={handleToggle}>
+                            toggle
+                        </button>
 
-        <button onClick={handleNextBtn}>
-          next
-        </button>
-    </div>
-</div>
+                    <div className="button-group">
+                        <button onClick={handlePrevBtn}>
+                            prev
+                        </button>
+                           <h2><span>{data?.page}</span>/<span>{data?.total_pages}</span></h2>
+                        <button onClick={handleNextBtn}>
+                          next
+                        </button>
+                    </div>
+                </div>
         
         <div className="cards-wrapper">
             {
-                data.Search?.map(item => {
+                data.results?.map(item => {
                       return (
-                          <div className="card" key={item?.imdbID}>
-                          <img src={item?.Poster} />
-                          <div className="text-wrapper">
-                              <h3>Title: {item?.Title}</h3>
-                              <p>Type: {item?.Type}</p>
-                              <p>Year: {item?.Year}</p>
+                  
+                            <div className="card" key={item?.id}>
+                            <img style={{width: "18.7rem"}} src={`${IMG_PATH}${item?.poster_path}`} />
+                            <div className="text-wrapper">
+                                <h3>Title: {item?.original_title}</h3>
+                                <div className="year-vote">
+                                    <p>Year: {item?.release_date}</p>
+                                    <h2 style={{color: `#${Math.random().toString(16).substring(2, 8)}`}}>
+                                       {item?.vote_average}
+                                    </h2>
+                                </div>
+                               
+                              <div className="item-desc">
+                                <h3>
+                                  overview
+                                </h3>
+                                <p>
+                                   {item?.overview}
+                                </p>
+                              </div>
+                        
+                            </div>
+
+                            
                           </div>
-                        </div>
+                      
                       )
                 })
             }
         </div>
-      </div>
+        </div>
     )
 }
 
